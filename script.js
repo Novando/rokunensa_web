a4865a4f8704e80d58a170f74674c35e77d3048b
Rizky Novando (Desktop) <rizkynovando@gmail.com>
2021-07-25 01:00:42 +0700

    product detail page created

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function menuToggle() {
 		MenuItems.style.maxHeight = "0px";
 	}
 }
+
+////// PRODUCT DETAIL PAGE'S SCRIPT //////
+var ProductImg = document.getElementById('product-img');
+var SmallImg = document.getElementsByClassName('small-img');
+
+SmallImg[0].onmouseover = function() {
+	ProductImg.src = SmallImg[0].src;
+}
+SmallImg[1].onmouseover = function() {
+	ProductImg.src = SmallImg[1].src;
+}
+SmallImg[2].onmouseover = function() {
+	ProductImg.src = SmallImg[2].src;
+}
+SmallImg[3].onmouseover = function() {
+	ProductImg.src = SmallImg[3].src;
+}
